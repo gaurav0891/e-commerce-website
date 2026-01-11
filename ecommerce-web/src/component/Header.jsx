4d320f84b1b05bbf555651e0b324ea405d4725fe
[Gaura Sharma] <gauravsharma272006@gmail.com>
2026-01-11 19:55:59 +0530

    feat: add Header component for navigation

---
+++ ecommerce-web/src/component/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+        <h1 className="logo">MyStore</h1>
+        <nav className="nav">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/products">Products</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+export default Header;
